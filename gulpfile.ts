a027006005fe36b2e5b06b0467e54a85997511f9
kolorobot <rafal.borowiec@gmail.com>
2016-03-04 00:12:42 +0100

    Added watch task and modified npm server script.

--- gulpfile.ts
+++ gulpfile.ts
@@ -60,6 +60,18 @@ gulp.task("libs", () => {
 });
 
 /**
+ * Watch for changes in TypeScript, HTML and CSS files.
+ */
+gulp.task('watch', function () {
+    gulp.watch(["src/**/*.ts"], ['compile']).on('change', function (e) {
+        console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+    });
+    gulp.watch(["src/**/*.html", "src/**/*.css"], ['resources']).on('change', function (e) {
+        console.log('Resource file ' + e.path + ' has been changed. Updating.');
+    });
+});
+
+/**
  * Build the project.
  */
 gulp.task("build", ['compile', 'resources', 'libs'], () => {
